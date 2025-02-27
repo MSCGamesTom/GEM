b5c356347235b95ee92db309c6306890c52839ab
MSCGamesTom <134287460+MSCGamesTom@users.noreply.github.com>
2025-02-27 23:18:16 +0000

    Tidying up of files

--- GEMScene/GEMScene.py
+++ GEMScene/GEMScene.py
@@ -4,13 +4,10 @@ import json
 class InlineListEncoder(json.JSONEncoder):
     def iterencode(self, o, _one_shot=False):
         if isinstance(o, list):
-            # Inline encode the list.
             return iter(['[' + ', '.join(self.encode(e) for e in o) + ']'])
         elif isinstance(o, dict):
-            # For dictionaries, mimic pretty printing with inline lists.
             if not o:
                 return iter(['{}'])
-            # inner_indent is used for the key-value pairs.
             inner_indent = ' ' * (self.indent) if self.indent is not None else ''
             items = []
             for key, value in o.items():
@@ -19,9 +16,7 @@ class InlineListEncoder(json.JSONEncoder):
                 else:
                     encoded_value = self.encode(value)
                 items.append(f'{json.dumps(key)}: {encoded_value}')
-            # Join the items with commas and newlines.
             joined_items = (',\n' + inner_indent).join(items)
-            # The final closing brace has no indent.
             return iter([f'{{\n{inner_indent}{joined_items}\n}}'])
         else:
             return super().iterencode(o, _one_shot)
@@ -51,16 +46,3 @@ class GEMScene:
         data = json.dumps(self.base, cls=InlineListEncoder, indent=4)
         with open(filename, "w") as file:
             file.write(data)
-
-##item = GEMSceneItem()
-##item.addFilename("a.gem")
-##item.addAttribute("bsdf", "diffuse")
-##item.addAttribute("diffuse", "albedo.png")
-##
-##scene = GEMScene()
-##scene.addSceneItem(item)
-##item.addFilename("b.gem")
-##scene.addSceneItem(item)
-##scene.addRendererItem("width", "1920")
-##scene.addRendererItem("height", "1080")
-##scene.save("scene1.json")
