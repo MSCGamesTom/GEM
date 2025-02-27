a177a7e27eac29e70360cd91f2098d7f0d949b96
MSCGamesTom <134287460+MSCGamesTom@users.noreply.github.com>
2025-02-27 23:16:22 +0000

    Cleaned up unnecessary files

--- GEMScene/MitsubaConverter/SkyCreator/Colour.cpp
+++
@@ -1,8 +0,0 @@
-#include "Colour.h"
-
-const Colour Colours::black = Colour(0.0f,0.0f,0.0f);
-const Colour Colours::white = Colour(1.0f,1.0f,1.0f);
-const Colour Colours::red = Colour(1.0f,0.0f,0.0f);
-const Colour Colours::green = Colour(0.0f,1.0f,0.0f);
-const Colour Colours::blue = Colour(0.0f,0.0f,1.0f);
-const Colour Colours::grey = Colour(0.5f,0.5f,0.5f);
